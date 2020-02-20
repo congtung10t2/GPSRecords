6938fbb240fdd2559a5def389c0529f90214ecbf
Hoang Cong Tung <congtung10t2@gmail.com>
2020-02-20 17:12:14 +0700

    Initial Commit

---
+++ GPSTracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GPSTracker
+//
+//  Created by iMac on 2/20/20.
+//  Copyright © 2020 iMac. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ GPSTracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GPSTracker
+//
+//  Created by iMac on 2/20/20.
+//  Copyright © 2020 iMac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
